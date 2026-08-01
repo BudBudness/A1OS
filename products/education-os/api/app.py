c344e10c5df1e19ddc7ffce29e4f5492885018f4
BudBudness <billionseddie@protonmail.com>
2026-08-02 02:35:35 +0300

    Little Oaks v3.9 release management production layer

--- products/education-os/api/app.py
+++ products/education-os/api/app.py
@@ -1,3 +1,4 @@
+from api.modules.live_operations.release_api import router as release_router
 from api.modules.live_operations.production_readiness_api import router as readiness_router
 from api.modules.live_operations.disaster_recovery_api import router as recovery_router
 from api.modules.live_operations.observability_api import router as observability_router
@@ -65,6 +66,7 @@ app.include_router(feature_router)
 app.include_router(observability_router)
 app.include_router(recovery_router)
 app.include_router(readiness_router)
+app.include_router(release_router)
 
 
 
