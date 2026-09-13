f8897cd3c944d3788101f2b46cf6ced16d9d6189
BudBudness <210061945+BudBudness@users.noreply.github.com>
2026-09-13 23:56:55 +0300

    fix: remove duplicate worker decorator

--- .private/core/worker.py
+++ .private/core/worker.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict
 
+
 class BaseWorker(ABC):
-    @abstractmethod
     @abstractmethod
     async def execute(self, event: Dict[str, Any]) -> Any:
         raise NotImplementedError
